c56af0485dcf4c06ae82e064b50e8f397e247395
Kateryna Kushnir <katerynakushnir3@gmail.com>
2024-09-16 22:55:45 +0100

    Created extended classes Publisher and Subscriber

--- eventBus.js
+++ eventBus.js
@@ -5,3 +5,19 @@ class Event {
         this.timestamp = Date.now();
     }
 }
+
+class Publisher extends Event {
+    constructor(eventType, deviceID, sensorType, value, unit) {
+        super(eventType, deviceID, this.timeStamp);
+        this.sensorType = sensorType;
+        this.value = value;
+        this.unit = unit;
+    }
+}
+
+class Subscriber extends Event {
+    constructor(eventType, deviceID, status) {
+        super(eventType, deviceID, this.timeStamp);
+        this.status = status;
+    }
+}
