7fb32aa2df34fb2bf223071fb0cb4915ed82e76c
Kateryna Kushnir <katerynakushnir3@gmail.com>
2024-09-18 11:01:48 +0100

    Reformed classes in eventBus.js

--- eventBus.js
+++ eventBus.js
@@ -7,14 +7,73 @@ class Event {
     }
 }
 
-class Publisher extends Event {
-    constructor(eventType, deviceID, data) {
-        super(eventType, deviceID, data, this.timeStamp);
+class Publisher {
+    constructor(eventBus) {
+        this.eventBus = eventBus;  // The EventBus instance for communication
+    }
+
+    publishEvent(eventType, deviceID, data) {
+        const event = new Event(eventType, deviceID, data);  // Create a new event
+        this.eventBus.publish(event);  // Publish event to the Event Bus
     }
 }
 
-class Subscriber extends Event {
-    constructor(eventType, deviceID, data) {
-        super(eventType, deviceID, data, this.timeStamp);
+class Subscriber {
+    constructor(eventBus, eventType, handler) {
+        this.eventBus = eventBus;
+        this.eventType = eventType;
+        this.handler = handler;
+
+        // Subscribe to the event type when the class is instantiated
+        this.eventBus.subscribe(eventType, handler);
     }
 }
+
+// The EventBus Class - Manages publishing and subscribing to events
+class EventBus {
+    constructor() {
+        this.subscribers = {};  // Store subscribers by event type
+    }
+
+    // Subscribe a handler to a specific event type
+    subscribe(eventType, handler) {
+        if (!this.subscribers[eventType]) {
+            this.subscribers[eventType] = [];
+        }
+        this.subscribers[eventType].push(handler);
+    }
+
+    // Publish an event to all relevant subscribers
+    publish(event) {
+        const eventType = event.eventType;
+        const handlers = this.subscribers[eventType];
+        if (handlers) {
+            handlers.forEach(handler => handler(event));
+        }
+    }
+}
+
+// Example usage:
+
+// Initialize the Event Bus
+const eventBus = new EventBus();
+
+// Create a Publisher
+const publisher = new Publisher(eventBus);
+
+// Create a Subscriber for temperature events
+const temperatureSubscriber = new Subscriber(eventBus, 'temperatureChange', (event) => {
+    console.log(`Temperature event received from Device ${event.deviceID}:`, event.data);
+});
+
+// Create a Subscriber for motion detection events
+const motionSubscriber = new Subscriber(eventBus, 'motionDetected', (event) => {
+    console.log(`Motion detected from Device ${event.deviceID}:`, event.data);
+});
+
+// Publisher publishes a temperature event
+publisher.publishEvent('temperatureChange', 'device123', { temperature: 22.5 });
+
+// Publisher publishes a motion detected event
+publisher.publishEvent('motionDetected', 'device456', { motionDetected: true });
+
